c71a5d8893eb4a8962433e1b1870c41bceafb1eb
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2018-10-15 20:02:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/luizpinheiro/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.luizpinheiro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizpinheiro.workshopmongo.domain.User;
+import com.luizpinheiro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
