fd314ec60c63ef7812db6baa03704620d788a4af
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2018-10-24 19:52:40 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luizpinheiro/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.luizpinheiro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizpinheiro.workshopmongo.domain.Post;
+import com.luizpinheiro.workshopmongo.repository.PostRepository;
+import com.luizpinheiro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
