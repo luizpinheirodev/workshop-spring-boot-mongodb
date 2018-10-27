118a79f232cb1a505bfa86d4853ee534a00d76dd
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2018-10-27 09:22:45 -0300

    Consulta simples com @Query

--- src/main/java/com/luizpinheiro/workshopmongo/services/PostService.java
+++ src/main/java/com/luizpinheiro/workshopmongo/services/PostService.java
@@ -22,8 +22,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public List<Post> findByTitle(String text){
+	/*public List<Post> findByTitle(String text){
 		return repo.findByTitleContainingIgnoreCase(text);
+	}*/
+	
+	public List<Post> findByTitle(String text){
+		return repo.searchTitle(text);
 	}
 
 	
